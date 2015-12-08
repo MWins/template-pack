c406f0dd3f1642e13b8f237fe29ec6db4177177b
Malcolm Winslett <malwinsc@yahoo.com>
2015-12-08 08:53:33 -0500

    Create namespace-example.js

---
+++ javascript/namespace-example.js
@@ -0,0 +1,44 @@
+/* source : http://www.alphansotech.com/blogs/object-oriented-programming-javascript-part-2/
+
+description : how to declare a global namespace in javascript. 
+*/
+
+var MYAPP = MYAPP || {};
+
+
+MYAPP.event = {};  // sub namespace
+
+
+// Create container called MYAPP.commonMethod for common method and properties
+
+MYAPP.commonMethod = {
+  regExForName: "", // define regex for name validation
+  regExForPhone: "", // define regex for phone no validation
+  validateName: function(name){
+    // Do something with name, you can access regExForName variable
+    // using "this.regExForName"
+  },
+ 
+  validatePhoneNo: function(phoneNo){
+    // do something with phone number
+  }
+}
+
+// Object together with the method declarations
+MYAPP.event = {
+    addListener: function(el, type, fn) {
+    //  code stuff
+    },
+   removeListener: function(el, type, fn) {
+    // code stuff
+   },
+   getEvent: function(e) {
+   // code stuff
+   }
+  
+   // Can add another method and properties
+}
+
+// Syntax for Using addListener method:
+MYAPP.event.addListener("yourel", "type", callback);
+
