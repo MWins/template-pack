67207625daa7e8dcef14508f2b07e60c4e5dcbd7
Malcolm Winslett <malwinsc@yahoo.com>
2016-03-20 22:05:46 -0400

    Create html5.tpl.php

---
+++ twig/html5.tpl.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>{{ title }}</title>
+  <meta name="author" content="{{ author_name }}">
+  <meta name="description" content="{{ description }}">
+  <meta name="keywords" content="{{ keywords }}">
+  <link rel="stylesheet" href="ui/insane.css" type="text/css">
+ </head>
+ <body>
+  {{ content }}
+ </body>
+</html>
